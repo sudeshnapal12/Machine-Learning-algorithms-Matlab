e59c42e57e96b39ef08009371955f5cdc28f07f0
Sudeshna Pal <sudeshnapal12@gmail.com>
2017-03-30 15:07:40 -0400

    Update main_rr.m

--- Kernel_Ridge_Regression/main_rr.m
+++ Kernel_Ridge_Regression/main_rr.m
@@ -1,23 +1,24 @@
 load adult.mat;
 % lambda = 0.002 and linear kernel 
-%------------Trainerror = 100-84.4800 Testerror=100-84.6077
+%------------Trainerror = 15.52 Testerror = 15.39 
 alpha = train_krr(Xtr, ytr, .002, 'linear', .001);
 ypredicted = test_krr(alpha, Xtr, ytr, Xte, .002, 'linear', .001);
 
 % lambda = 0.002 and gaussian kernel 
-%-----------Testerror = 100-75.96 Trainerror =
+%------------Trainerror = 25.2800 Testerror = 24.04 
+%-----probably overfit happened using gaussian kernel with same lambda
 %alpha = train_krr(Xtr, ytr, .002, 'gaussain', .001);
 %ypredicted = test_krr(alpha, Xtr, ytr, Xte, .002, 'gaussian', .001);
 
-% lambda = 2e - 05 and linear kernel
-%-----------Testerror =  Trainerror =
+% lambda = 2e - 05 = .00002 and linear kernel
+%----------- Trainerror = 15.52 Testerror = 15.3923
 %alpha = train_krr(Xtr, ytr, .00002, 'linear', .001);
-%ypredicted = test_krr(alpha, Xtr, ytr, Xte, .002, 'linear', .001);
+%ypredicted = test_krr(alpha, Xtr, ytr, Xte, .00002, 'linear', .001);
 
-% lambda = 2e - 05 and gaussian kernel
-%-----------Testerror =  Trainerror =
+% lambda = 2e - 05 = .00002 and gaussian kernel
+%-----------Trainerror = 9.9400 Testerror = 15.6422 
 %alpha = train_krr(Xtr, ytr, .00002, 'gaussian', .001);
-%ypredicted = test_krr(alpha, Xtr, ytr, Xte, .002, 'linear', .001);
+%ypredicted = test_krr(alpha, Xtr, ytr, Xte, .00002, 'gaussian', .001);
 
 count = 0;
 for i = 1 : size(ypredicted,1)
