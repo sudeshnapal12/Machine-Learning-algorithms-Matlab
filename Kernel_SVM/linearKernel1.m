2a824c23a768b8b6b42d0f85382d5075bebbf935
Sudeshna Pal <sudeshnapal12@gmail.com>
2017-03-31 00:22:06 -0400

    Delete linearKernel1.m

--- Kernel_SVM/linearKernel1.m
+++
@@ -1,8 +0,0 @@
-% Linear Kernel is like a dummy kernel in which the transformed feature
-% space is same as the original feature space.
-% Linear Kernel returns the similarity betweeen x1 and x2.
-function K = linearKernel(x1, x2)
-
-    K = x1 * x2';  % dot product
-
-end
